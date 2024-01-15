79e8476b760f5e4cf6cc2e0e247aa8663ac0cda1
minzion <aert12435@gmail.com>
2024-01-16 02:45:31 +0900

    #16 - 게시판 view 기본 바탕 작성
    컨트롤러 핸들러가 메소드로 테스트를 통과하게 열어주고
    뷰데이터 뼈대를 잡음

--- src/test/java/com/study/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/study/projectboard/controller/ArticleControllerTest.java
@@ -20,7 +20,7 @@ class ArticleControllerTest {
     public ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
-    @Disabled("구현중")
+
     @DisplayName("[view][Get] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticleleView_then()throws Exception{
@@ -29,7 +29,7 @@ class ArticleControllerTest {
         //when.then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())//200으로 오는지 확인
-                .andExpect(content().contentType(MediaType.TEXT_HTML))//문자 타입으로 오는지 확인
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))//문자 타입으로 오는지 확인
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));//넘어오는 model 안에 articles의 데이터가 넘어오는지.
     }
@@ -43,7 +43,7 @@ class ArticleControllerTest {
         //when.then
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())//200으로 오는지 확인
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
@@ -57,7 +57,7 @@ class ArticleControllerTest {
         //when.then
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())//200으로 오는지 확인
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("articles/search"));
     }
     @Disabled("구현중")
@@ -69,7 +69,7 @@ class ArticleControllerTest {
         //when.then
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())//200으로 오는지 확인
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("articles/search-hashtag"));
     }
 }
