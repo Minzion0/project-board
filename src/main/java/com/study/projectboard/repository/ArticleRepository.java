94a84aa5dd39597b5f24b7085c2b56e47e4ccb25
minzion <aert12435@gmail.com>
2024-01-15 18:43:26 +0900

    #1 - repository에 어노테이션 추가
    게시글 댓글에 REST api 설정

--- src/main/java/com/study/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/study/projectboard/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.study.projectboard.repository;
 
 import com.study.projectboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource// spring data rest 를 사용하기 위해 repository 에 해당 어노테이션을 넣어준다.
 public interface ArticleRepository extends JpaRepository<Article,Long> {
 }
