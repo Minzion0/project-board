91e495336095e35f18beaf82eea29845b43a0444
minzion <aert12435@gmail.com>
2024-03-27 18:10:44 +0900

    [리팩토링]AuditingFields 접근제시어 수정 및 추상 클래스 설정 누락 수정

--- src/main/java/com/study/projectboard/domain/AuditingFields.java
+++ src/main/java/com/study/projectboard/domain/AuditingFields.java
@@ -20,23 +20,23 @@ import java.time.LocalDateTime;
 // 사용해준다는 표시 이것을 해주어야
 //modifiedBy 나 그런것들이 자동으로 입력된다.
 @MappedSuperclass// entity 상속해줄 클래스에 사용하는 어노테이션
-public class AuditingFields {
+public abstract class AuditingFields {
 
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)//date time 포맷을 사용한다는 설정
     @CreatedDate
     @Column(nullable = false,updatable = false)
-    private LocalDateTime createdAt;//생성일시
+    protected LocalDateTime createdAt;//생성일시
 
     @CreatedBy
     @Column(nullable = false,length = 100,updatable = false)
-    private String createdBy;//생성자
+    protected String createdBy;//생성자
 
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     @LastModifiedDate
     @Column(nullable = false)
-    private LocalDateTime modifiedAt;//수정일시
+    protected LocalDateTime modifiedAt;//수정일시
 
     @LastModifiedBy
     @Column(nullable = false,length = 100)
-    private String modifiedBy;//수정자
+    protected String modifiedBy;//수정자
 }
